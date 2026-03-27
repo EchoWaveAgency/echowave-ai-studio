5dcf2a8e55f6b6d03d34709a52841bc4faba0db3
EchoWaveAgency <echowaveagency0@gmail.com>
2026-03-27 03:06:32 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'], // هنضيف الدومين بتاعك هنا لما يجهز
+  },
+}
+
+module.exports = nextConfig
